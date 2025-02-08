14b844f9e6de2b13d5bebdce443d1daa5ff2a796
CarlGarcia97 <carlosvigar97@gmail.com>
2025-02-08 19:19:47 +0100

    borrado atributo acertado

--- DondeEstoyGame/app/src/main/java/com/example/dondeestoygame/modelo/ComidaSurtidor.kt
+++ DondeEstoyGame/app/src/main/java/com/example/dondeestoygame/modelo/ComidaSurtidor.kt
@@ -7,11 +7,11 @@ object ComidaSurtidor {
     private val listComida = ArrayList<Comida>()
 
     fun getComida():ArrayList<Comida>{
-        listComida.add(Comida("Migas de pastor", R.drawable.migas, 0, 0, 0.00, false))
-        listComida.add(Comida("Sopa de ajo", R.drawable.sopa_castellana, 0, 0, 0.00, false))
-        listComida.add(Comida("Cocas", R.drawable.cocas, 0, 0, 0.00, false))
-        listComida.add(Comida("Salmorejo", R.drawable.salmorejo, 0, 0, 0.00, false))
-        listComida.add(Comida("Zarangollo", R.drawable.zarangollo, 0, 0, 0.00, false))
+        listComida.add(Comida("Migas de pastor", R.drawable.migas, 0, 0, 0.00))
+        listComida.add(Comida("Sopa de ajo", R.drawable.sopa_castellana, 0, 0, 0.00))
+        listComida.add(Comida("Cocas", R.drawable.cocas, 0, 0, 0.00))
+        listComida.add(Comida("Salmorejo", R.drawable.salmorejo, 0, 0, 0.00))
+        listComida.add(Comida("Zarangollo", R.drawable.zarangollo, 0, 0, 0.00))
 
         return listComida
     }
