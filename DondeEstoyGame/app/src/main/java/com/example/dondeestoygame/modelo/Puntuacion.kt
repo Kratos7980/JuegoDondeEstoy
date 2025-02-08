e31b0f0647d4a02ea9806fabcf70ec6f4f28e14a
CarlGarcia97 <carlosvigar97@gmail.com>
2025-02-08 17:13:40 +0100

    implementacion claseObject puntuacion

--- DondeEstoyGame/app/src/main/java/com/example/dondeestoygame/modelo/Puntuacion.kt
+++ DondeEstoyGame/app/src/main/java/com/example/dondeestoygame/modelo/Puntuacion.kt
@@ -2,5 +2,27 @@ package com.example.dondeestoygame.modelo
 
 object Puntuacion {
 
-    
+    private var puntos:Int
+    private var intentos:Int
+
+    init {
+        puntos = 0
+        intentos = 5
+    }
+
+    fun getPuntos():Int{
+        return puntos
+    }
+
+    fun sumarPuntos(puntos:Int){
+        this.puntos += puntos
+    }
+
+    fun setIntentos(intentos:Int){
+        this.intentos = intentos
+    }
+
+    fun getIntentos():Int{
+        return intentos
+    }
 }
