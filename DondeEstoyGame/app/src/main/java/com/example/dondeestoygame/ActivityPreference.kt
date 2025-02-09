f86ca82b14d7202fe6588007a95a6c85103fe839
PabloMartinVR <pablovelascoruiz68@gmail.com>
2025-02-09 12:21:33 +0100

    Hecho la pantalla de preferencias.

---
+++ DondeEstoyGame/app/src/main/java/com/example/dondeestoygame/ActivityPreference.kt
@@ -0,0 +1,22 @@
+package com.example.dondeestoygame
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.dondeestoygame.databinding.ActivityPreferenceBinding
+
+class ActivityPreference : AppCompatActivity() {
+
+    lateinit var binding: ActivityPreferenceBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityPreferenceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
