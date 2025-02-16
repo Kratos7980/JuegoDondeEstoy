f27014d6a59b44854c1be021c1cd1efda649bd62
CarlGarcia97 <carlosvigar97@gmail.com>
2025-02-16 18:21:51 +0100

    retoques pantalla principal

--- DondeEstoyGame/app/src/main/java/com/example/dondeestoygame/view/AcercaDeActivity.kt
+++ DondeEstoyGame/app/src/main/java/com/example/dondeestoygame/view/AcercaDeActivity.kt
@@ -1,21 +1,16 @@
 package com.example.dondeestoygame.view
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.dondeestoygame.R
+import com.example.dondeestoygame.databinding.ActivityAcercaDeBinding
 
 class AcercaDeActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityAcercaDeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_acerca_de)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityAcercaDeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
